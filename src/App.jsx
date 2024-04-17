1684b9cd36f546e52d0ab8b599699b73ca28cf77
luizGMPortfolio <luizgm.dev@gmail.com>
2024-04-17 18:13:40 -0300

    versão 3

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,28 @@ function App() {
     const [error, setError] = useState(null); // Estado para armazenar um possível erro
 
     const [ref, inView] = useInView({
-        threshold: 0 // 0 significa que o callback será executado assim que um pixel for visível
+        threshold: 1 // 0 significa que o callback será executado assim que um pixel for visível
     });
 
     const [show, setShow] = useState('');
     const [type, setType] = useState('type');
-    const [Gen, setGen] = useState(0);
+    const [Gen, setGen] = useState('Generations');
     const [Order, setOrder] = useState('Order');
     const [name, setName] = useState('');
 
 
-
+    function reset() {
+        setType('type');
+        setGen('Generations');
+        setRendersMax(0);
+        setRendersMin(-1);
+        setPokemons([]);
+    }
 
     const fetchData = async () => {
         try {
             // Fazendo a requisição para a API e armazenando a resposta
             var info = [];
-
             for (let j = Gens[RendersMin] + 1; j <= Gens[RendersMax]; j++) {
 
                 const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + j + "/");
@@ -46,8 +51,7 @@ function App() {
                     });
                 }
             }
-            setPokemons(info);
-            setAllPokemons(pokemons);
+            setPokemons([...pokemons, ...info]);
         } catch (error) {
             setError(error); // Armazena qualquer erro que ocorra
         }
@@ -56,18 +60,60 @@ function App() {
     useEffect(() => {
         const handleScroll = async () => {
             if (inView) {
-                setRendersMax(RendersMax + 1)
+                if (RendersMax <= 9) {
+                    setRendersMin(RendersMax)
+                    setRendersMax(RendersMax + 1)
+                    fetchData();
+                }
             }
         }
         handleScroll();
     }, [inView]);
 
+    function fetchType(type) {
+        setType(type);
+        setPokemons([]);
+    }
+    function fetchGen(Gen) {
+        setGen(Gen);
+        setPokemons([]);
+    }
+    useEffect(() => {
+        if(Gen === 'Generations'){
+            setRendersMin(0)
+            setRendersMax(1)
+            setPokemons([]);
+        }
+        else{
+            setRendersMin(Gen - 1)
+            setRendersMax(Gen)
+        }
+    }, [Gen]);
 
     useEffect(() => {
-        fetchData(); // Chama a função fetchData ao montar o componente
-    }, [RendersMax, RendersMin, type, Order]);
+        setPokemons([]);
+        fetchName();
+    }, [name]);
 
+    const fetchName = async () => {
+        if (name === '') {
+            reset();
+            fetchData();
+        }
+        else {
 
+            try {
+                // Fazendo a requisição para a API e armazenando a resposta
+                var info = [];
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + name + "/");
+                info.push(response.data);
+                setPokemons(info);
+
+            } catch (error) {
+                setError(error); // Armazena qualquer erro que ocorra
+            }
+        }
+    }
 
 
     return (
@@ -97,42 +143,42 @@ function App() {
                             <h4>{type}</h4>
                         </div>
                         <ul name="options" id="options" className={`options ${show === 'type' ? 'show' : ''}`}>
-                            <li value="water" onClick={() => setType('type')} className='type'>type</li>
-                            <li value="water" onClick={() => setType('water')} className='water'>water</li>
-                            <li value="glass" onClick={() => setType('grass')} className='grass'>grass</li>
-                            <li value="fire" onClick={() => setType('fire')} className='fire'>fire</li>
-                            <li value="fire" onClick={() => setType('electric')} className='electric'>electric</li>
-                            <li value="Flying" onClick={() => setType('flying')} className='flying'>Flying</li>
-                            <li value="Fighting" onClick={() => setType('fighting')} className='fighting'>Fighting</li>
-                            <li value="Poison" onClick={() => setType('poison')} className='poison'>Poison</li>
-                            <li value="Ground" onClick={() => setType('ground')} className='ground'>Ground</li>
-                            <li value="Rock" onClick={() => setType('rock')} className='rock'>Rock</li>
-                            <li value="Psychic" onClick={() => setType('psychic')} className='psychic'>Psychic</li>
-                            <li value="Ice" onClick={() => setType('ice')} className='ice'>Ice</li>
-                            <li value="Bug" onClick={() => setType('bug')} className='bug'>Bug</li>
-                            <li value="Ghost" onClick={() => setType('ghost')} className='ghost'>Ghost</li>
-                            <li value="Steel" onClick={() => setType('ateel')} className='steel'>Steel</li>
-                            <li value="Dragon" onClick={() => setType('dragon')} className='dragon'>Dragon</li>
-                            <li value="Dark" onClick={() => setType('dark')} className='dark'>Dark</li>
-                            <li value="Fairy" onClick={() => setType('fairy')} className='fairy'>Fairy </li>
+                            <li value="water" onClick={() => fetchType('type')} className='type'>type</li>
+                            <li value="water" onClick={() => fetchType('water')} className='water'>water</li>
+                            <li value="glass" onClick={() => fetchType('grass')} className='grass'>grass</li>
+                            <li value="fire" onClick={() => fetchType('fire')} className='fire'>fire</li>
+                            <li value="fire" onClick={() => fetchType('electric')} className='electric'>electric</li>
+                            <li value="Flying" onClick={() => fetchType('flying')} className='flying'>Flying</li>
+                            <li value="Fighting" onClick={() => fetchType('fighting')} className='fighting'>Fighting</li>
+                            <li value="Poison" onClick={() => fetchType('poison')} className='poison'>Poison</li>
+                            <li value="Ground" onClick={() => fetchType('ground')} className='ground'>Ground</li>
+                            <li value="Rock" onClick={() => fetchType('rock')} className='rock'>Rock</li>
+                            <li value="Psychic" onClick={() => fetchType('psychic')} className='psychic'>Psychic</li>
+                            <li value="Ice" onClick={() => fetchType('ice')} className='ice'>Ice</li>
+                            <li value="Bug" onClick={() => fetchType('bug')} className='bug'>Bug</li>
+                            <li value="Ghost" onClick={() => fetchType('ghost')} className='ghost'>Ghost</li>
+                            <li value="Steel" onClick={() => fetchType('steel')} className='steel'>Steel</li>
+                            <li value="Dragon" onClick={() => fetchType('dragon')} className='dragon'>Dragon</li>
+                            <li value="Dark" onClick={() => fetchType('dark')} className='dark'>Dark</li>
+                            <li value="Fairy" onClick={() => fetchType('fairy')} className='fairy'>Fairy </li>
                         </ul>
                     </div>
 
                     <div className='f-generations' onMouseMove={() => setShow('Generations')} onMouseLeave={() => setShow('')}>
                         <div className={`select ${Gen != 0 ? Gen : ''} Generations`}>
-                            <h4>{Gen != 0 ? `${Gen}° Gen` : 'Generations'}</h4>
+                            <h4>{Gen != 'Generations' ? `${Gen}° Gen` : 'Generations'}</h4>
                         </div>
                         <ul name="options" id="options" className={`options ${show === 'Generations' ? 'show' : ''}`}>
-                            <li value="1" onClick={() => setRenders(1)} className=''>Generations</li>
-                            <li value="1" onClick={() => setRenders(1)} className=''>1° Gen</li>
-                            <li value="2" onClick={() => setRenders(2)} className=''>2° Gen</li>
-                            <li value="3" onClick={() => setRenders(3)} className=''>3° Gen</li>
-                            <li value="4" onClick={() => setRenders(4)} className=''>4° Gen</li>
-                            <li value="5" onClick={() => setRenders(5)} className=''>5° Gen</li>
-                            <li value="6" onClick={() => setRenders(6)} className=''>6° Gen</li>
-                            <li value="7" onClick={() => setRenders(7)} className=''>7° Gen</li>
-                            <li value="8" onClick={() => setRenders(8)} className=''>8° Gen</li>
-                            <li value="9" onClick={() => setRenders(9)} className=''>9° Gen</li>
+                            <li value="1" onClick={() => fetchGen('Generations')} className=''>Generations</li>
+                            <li value="1" onClick={() => fetchGen(1)} className=''>1° Gen</li>
+                            <li value="2" onClick={() => fetchGen(2)} className=''>2° Gen</li>
+                            <li value="3" onClick={() => fetchGen(3)} className=''>3° Gen</li>
+                            <li value="4" onClick={() => fetchGen(4)} className=''>4° Gen</li>
+                            <li value="5" onClick={() => fetchGen(5)} className=''>5° Gen</li>
+                            <li value="6" onClick={() => fetchGen(6)} className=''>6° Gen</li>
+                            <li value="7" onClick={() => fetchGen(7)} className=''>7° Gen</li>
+                            <li value="8" onClick={() => fetchGen(8)} className=''>8° Gen</li>
+                            <li value="9" onClick={() => fetchGen(9)} className=''>9° Gen</li>
                         </ul>
                     </div>
 
